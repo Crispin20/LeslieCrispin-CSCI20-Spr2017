579002e15a074019efdcc989d8f3d4b88aa874b7
Leslie Crispin <lcrispin001@student.butte.edu>
2017-01-31 21:51:40 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,8 @@
+/* 
+1.  Have the computer ramdomly choose one of two numbers between 1 and 10.
+2.  Them prompt the user to guess what that number is.  Their starting score is 10.
+    a. Each time a user guesses a number too low, then the program tells them to guess higher and one point is subtracted from the score. 
+    b. Each time a user guesses a number too high, then the program tells them to guess lower and one point is subtracted from the score.
+3.  This continues until the user guesses the number right and then the game is over. 
+
+*/
