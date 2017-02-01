8926cb3391ce9cb8671ea60fc43c4022d2ed280b
Leslie Crispin <lcrispin001@student.butte.edu>
2017-02-01 03:05:08 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,32 @@
+//Created by: Leslie Crispin 
+// Created on: 1-31-17
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "       _____   " << endl;
+    cout << "    .00000000000.  " << endl;
+    cout << "  .000000000000000. " << endl; 
+    cout << "  b000000000000000d  " << endl;
+    cout << " 000000000000000000 " << endl;
+    cout << " 000000000000000000 " << endl;
+    cout << " 000000000000000000 " << endl; 
+    cout << "  b00000000000000d " << endl;
+    cout << "  .00000000000000. " << endl;
+    cout << "    .0000000000.   " << endl;
+    cout << "      .000000.     " << endl;
+    cout << "        .000.      " <<endl;
+    cout << "          .0.      " << endl;
+    cout << "           0       " << endl;
+    cout << "            0      " << endl; 
+    cout << "            0      " << endl;
+    cout << "           0       " << endl;
+    cout << "          0        " << endl; 
+    cout << "         0         " << endl;
+    cout << "        0          " << endl; 
+    cout << "         0         " << endl;
+    cout << "          0        " << endl;
+    cout << "            0      " << endl; 
+    return 0;  
+}
